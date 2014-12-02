052cb9bdeb297c0e0f308e37dd616f8fb5b6afa9
evoup <evoex123@gmail.com>
2014-12-02 14:55:46 +0800

    c

--- helloWorld/src/com/evoup/helloworld.scala
+++ helloWorld/src/com/evoup/helloworld.scala
@@ -12,6 +12,14 @@ object helloworld {
     // var name = "evoup"
     // println(f"$name%s is $height%2.2f $s%d meters tail")
     val d = {val x=1;val y=2;sqrt(x+y)}
-    //printf(d;)
+    printf("Hello, %s! You are %d years old.\n","evoup",34)
+    for (i<- 1 to 10) {
+      printf("i:%d",i)
+    }
+    s=1+2+ //长语句
+    3
+    printf("\n--------------\ns:%d",s)
+    val distance={val dx=1;val dy=2;sqrt(dx+dy)}
+    printf("\n--------------\ndistance:%s",distance)
   }
 }
