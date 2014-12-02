2338893583df3ccba4ac0d54b1fea1bef3ff6f4f
evoup <evoex123@gmail.com>
2014-12-02 14:30:25 +0800

    add files

---
+++ helloWorld/src/com/evoup/helloworld.scala
@@ -0,0 +1,17 @@
+package com.evoup
+import scala.math._
+/**
+ * Created by yinjia on 2014/12/2.
+ */
+object helloworld {
+  def main(args: Array[String]): Unit = {
+    var x =147
+    var s = if(x>0) x else -1
+    println("hello world")
+    // val height = 1.9d
+    // var name = "evoup"
+    // println(f"$name%s is $height%2.2f $s%d meters tail")
+    val d = {val x=1;val y=2;sqrt(x+y)}
+    //printf(d;)
+  }
+}
